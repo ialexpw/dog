4be3399ad984dd928ca09822e65b1525aa467647
Alex <alex@picotory.com>
2018-09-24 15:36:07 +0100

    Init DB

---
+++ database.sql
@@ -0,0 +1,25 @@
+CREATE TABLE records (
+	id integer PRIMARY KEY AUTOINCREMENT,
+	response varchar,
+	timestamp varchar,
+	m_id varchar
+);
+
+CREATE TABLE users (
+	id integer PRIMARY KEY AUTOINCREMENT,
+	username varchar,
+	password varchar,
+	nti_email varchar,
+	nti_phone varchar,
+	active integer
+);
+
+CREATE TABLE monitors (
+	id integer PRIMARY KEY AUTOINCREMENT,
+	name varchar,
+	description varchar,
+	address varchar,
+	location varchar,
+	public integer,
+	u_id integer
+);
