0c50ce64c45be2291f70e37709c820b08f680618
Alex <alex@picotory.com>
2018-09-24 15:34:23 +0100

    Add needed functions/db link

--- app/dog.app.php
+++ app/dog.app.php
@@ -31,6 +31,44 @@
         }
 
         /**
+         * Dog::InstallDatabase()
+         * Install the database tables needed for tellDog
+         */
+        public static function InstallDatabase() {
+            $db = new SQLite3('app/db/dog.db', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
+
+            $db->query('CREATE TABLE users (
+                id integer PRIMARY KEY AUTOINCREMENT,
+                username varchar,
+                password varchar,
+                nti_email varchar,
+                nti_phone varchar,
+                active integer
+            )');
+            
+            $db->query('CREATE TABLE monitors (
+                id integer PRIMARY KEY AUTOINCREMENT,
+                name varchar,
+                description varchar,
+                address varchar,
+                location varchar,
+                public integer,
+                u_id integer
+                FOREIGN KEY(u_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE
+            )');
+            
+            $db->query('CREATE TABLE records (
+                id integer PRIMARY KEY AUTOINCREMENT,
+                response varchar,
+                timestamp varchar,
+                m_id varchar
+                FOREIGN KEY(m_id) REFERENCES monitors(id) ON UPDATE CASCADE ON DELETE CASCADE
+            )');
+
+            $db->close();
+        }
+
+        /**
          * Dog::LoggedIn()
          * Check logged in status by looking at the sessions
          */
@@ -40,6 +78,33 @@
 			}else{
 				return 1;
 			}
+        }
+
+        /**
+         * Dog::PingDomain($domain, $ssl)
+         * Ping a domain and return the response time in ms
+         */
+        public static function PingDomain($domain, $ssl) {
+            $sTime = microtime(true);
+
+            if($ssl) {
+                $fsOpen = @fsockopen($domain, 443, $errNo, $errstr, 2);
+            }else{
+                $fsOpen = @fsockopen($domain, 80, $errNo, $errstr, 2);
+            }
+
+            $eTime = microtime(true);
+
+            # Site is down
+            if(!$fsOpen) {
+                $status = -1;
+            }else{
+                $status = floor(($stoTime - $staTime) * 1000);
+            }
+
+            @fclose($fsOpen);
+
+            return $status;
         }
 
         /**
@@ -63,6 +128,46 @@
                 echo $lang[$string];
             }else{
                 echo 'TRANSLATION-ERROR';
+            }
+        }
+
+        /**
+         * Dog::Version()
+         * Return the current version of ViroCMS
+         */
+        public static function Version() {
+            return "v0.1.0-alpha";
+        }
+
+        /**
+         * Dog::CheckUpdate()
+         * Check if updates are available using Dog::Version() and the website
+         */
+        public static function CheckUpdate() {
+            # Get the current version
+            $getVer = file_get_contents('https://viro.app/dog-version.txt');
+
+            # Get our version
+            $locVer = explode('-', Dog::Version());
+            $locVer = str_replace('v', '', $locVer[0]);
+
+            # true = update available
+            if($getVer != $locVer) {
+                return true;
+            }else{
+                return false;
+            }
+        }
+
+        /**
+         * Dog::Installed()
+         * Check the installation status
+         */
+        public static function Installed() {
+            if(!file_exists('app/db/dog.db')) {
+                return false;
+            }else{
+                return true;
             }
         }
     }
