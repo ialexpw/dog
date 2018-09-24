a575a3c7d3125807baaa04ae49f6f17cc741453d
Alex <alex@picotory.com>
2018-09-24 15:35:57 +0100

    Init cron

---
+++ cron.php
@@ -0,0 +1,19 @@
+<?php
+    /**
+     * cron.php
+     *
+     * Main cron to poll servers
+     *
+     * @package    tellDog
+     * @author     Alex White (https://github.com/ialexpw)
+     * @copyright  2018 tellDog
+     * @license    https://github.com/ialexpw/dog/blob/master/LICENSE  MIT License
+     * @link       https://viro.app
+     */
+
+    include 'app/dog.app.php';
+
+    if(php_sapi_name() == "cli") {
+        
+    }
+?>
