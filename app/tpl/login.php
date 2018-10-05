ebb33f14b87a9cf5d797464d37ecc89b4f22cbf8
Alex <alex@picotory.com>
2018-10-05 16:48:14 +0100

    Update login page

--- app/tpl/login.php
+++ app/tpl/login.php
@@ -55,26 +55,159 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="robots" content="index, follow">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>tellDog - <?php Dog::Translate('Login', $l); ?></title>
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="app/tpl/css/bootstrap.min.css">
-    </head>
+<head>
+    <!-- Meta tags -->
+    <meta charset="utf-8">
+    <meta name="robots" content="index, follow">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-    <body>
-        <nav class="navbar navbar-dark bg-primary">
-            <!-- Navbar content -->
-        </nav>
-        <div class="container">
-            <h3>Test</h3>
+    <!-- Title -->
+    <title>tellDog - <?php Dog::Translate('dashboard', $l); ?></title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="favicon.ico">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="app/tpl/css/bootstrap.min.css">
+    <link rel="stylesheet" href="app/tpl/css/dashboard.css">
+
+    <!-- Javascript -->
+    <script type="text/javascript" src="app/tpl/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
+        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">tell<b>Dog</b></a>
+        <!--<input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">-->
+        <ul class="navbar-nav px-3">
+            <li class="nav-item text-nowrap">
+                <a class="nav-link" href="?logout">Sign out</a>
+            </li>
+        </ul>
+    </nav>
+
+    <div class="container-fluid">
+        <div class="row">
+            <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+                <div class="sidebar-sticky"><br /><br /><br />
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="?page=dashboard">
+                                <span data-feather="home"></span>
+                                Dashboard <span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+                    </ul>
+
+                    <hr>
+
+                    <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+                        <span>Server Monitoring</span>
+                        <a class="d-flex align-items-center text-muted" href="#">
+                            <span data-feather="menu"></span>
+                        </a>
+                    </h6>
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link" href="?page=add-server">
+                                <span data-feather="plus-square"></span>
+                                Add new
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="?page=manage-server">
+                                <span data-feather="hard-drive"></span>
+                                Manage
+                            </a>
+                        </li>
+                    </ul>
+
+                    <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+                        <span>External Monitors</span>
+                        <a class="d-flex align-items-center text-muted" href="#">
+                            <span data-feather="menu"></span>
+                        </a>
+                    </h6>
+                    <ul class="nav flex-column mb-2">
+                        <li class="nav-item">
+                            <a class="nav-link" href="?page=add-external">
+                                <span data-feather="plus-square"></span>
+                                Add new
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="?page=manage-external">
+                                <span data-feather="external-link"></span>
+                                Manage
+                            </a>
+                        </li>
+                    </ul>
+
+                    <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+                        <span>Tools</span>
+                        <a class="d-flex align-items-center text-muted" href="#">
+                            <span data-feather="menu"></span>
+                        </a>
+                    </h6>
+                    <ul class="nav flex-column mb-2">
+                        <li class="nav-item">
+                            <a class="nav-link" href="?page=profile">
+                                <span data-feather="user"></span>
+                                Profile
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="?page=audit-log">
+                                <span data-feather="file-text"></span>
+                                Audit Log
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+            <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
+                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
+                    <h1 class="h2">Log in</h1>
+                </div>
+
+                <form action="?page=login" method="post">
+                    <div class="form-group row">
+                        <label for="username" class="col-sm-1 col-form-label">Username</label>
+                        <div class="col-sm-6">
+                            <input type="text" class="form-control" id="username" name="username" aria-describedby="UserHelp" placeholder="Enter username">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="password" class="col-sm-1 col-form-label">Password</label>
+                        <div class="col-sm-6">
+                            <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-sm-6">
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </div>
+                    </div>
+                </form>
+            </main>
         </div>
-        <div>
-            &copy; 2018 tellDog - <?php echo Dog::Version(); ?>.
-        </div>
-    </body>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+
+    <!-- Icons -->
+    <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
+    <script>
+        feather.replace()
+    </script>
+    <div>
+        &copy; 2018 tellDog - <?php echo Dog::Version(); ?>.
+    </div>
+</body>
+
 </html>
