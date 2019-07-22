e9f6a528fbf051e12cca9a93349320176f2dccae
Alex White <alex@picotory.com>
2019-07-22 16:06:29 +0100

    ~

--- app/tpl/manage-external.php
+++ app/tpl/manage-external.php
@@ -147,7 +147,7 @@
 
 
                         <!--
-                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         <a href="#" class="card-link">Card link</a>
                         <a href="#" class="card-link">Another link</a>-->
@@ -158,7 +158,8 @@
                         echo '<div class="col-md-3">';
                         echo '<div class="card" style="width: 18rem;">';
                         echo '<div class="card-body">';
-                        echo '<h5 class="card-title">' . $ExMon['loc'] . '</h5>';
+                        echo '<h5 class="card-title">' . $ExMon['loc'] . ' (' . strtoupper($ExMon['code']) . ')</h5>';
+                        echo '<h6 class="card-subtitle mb-2 text-muted">' . $ExMon['addr'] . '</h6>';
                         echo '<a href="#" class="card-link">Card link</a>';
                         echo '<a href="#" class="card-link">Another link</a>';
                         echo '</div>';
