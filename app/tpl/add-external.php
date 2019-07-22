07fc7d8285af3bcef74028730a47e4a1419c88db
Alex White <alex@picotory.com>
2019-07-22 15:02:11 +0100

    Add external update

--- app/tpl/add-external.php
+++ app/tpl/add-external.php
@@ -147,19 +147,26 @@
 
                 <hr>
 
-                <form action="?page=login" method="post">
+                <form action="?page=add-external&add" method="post">
                     <div class="form-group row">
-                        <label for="username" class="col-sm-1 col-form-label">Username</label>
                         <div class="col-sm-6">
-                            <input type="text" class="form-control" id="username" name="username" aria-describedby="UserHelp" placeholder="Enter username">
+                            <label for="sname">Where is the external server located?</label>
+                            <input type="text" class="form-control" id="sname" name="sname" placeholder="Paris, France">
                         </div>
                     </div>
+
                     <div class="form-group row">
-                        <label for="password" class="col-sm-1 col-form-label">Password</label>
                         <div class="col-sm-6">
-                            <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                            <label for="saddress">What's the server address?</label>
+                            <div class="input-group mb-2">
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text">http(s)://</div>
+                                </div>
+                                <input type="text" class="form-control" id="saddress" name="saddress" placeholder="fr.example.com">
+                            </div>
                         </div>
                     </div>
+
                     <div class="form-group row">
                         <div class="col-sm-6">
                             <button type="submit" class="btn btn-primary">Submit</button>
