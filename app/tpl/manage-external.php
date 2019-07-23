71439b7670441ec89efcba9d9a1e4911a1928f5c
Alex White <alex@picotory.com>
2019-07-23 11:02:35 +0100

    Cleanup

--- app/tpl/manage-external.php
+++ app/tpl/manage-external.php
@@ -145,12 +145,6 @@
 
                 <hr>
 
-
-                        <!--
-
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="card-link">Card link</a>
-                        <a href="#" class="card-link">Another link</a>-->
                 <?php
                     echo '<div class="row">';
 
